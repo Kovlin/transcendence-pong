1d7757318c145f96342f1da9fef73cbef1d67f23
Kovlin <romainlink@hotmail.com>
2022-06-20 18:02:23 +0200

    test

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,14 @@
 import { Expression } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 
 import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { HostListener } from '@angular/core';
 
-const BOARD_W = 400;
-const BOARD_H = 800;
+// import { Subscription, timer, map } from 'rxjs';
+
+var BOARD_W = 400;
+var BOARD_H = 800;
+var speed = 20;
 
 @Component({
   selector: 'app-game',
@@ -19,46 +22,74 @@ export class GameComponent implements AfterViewInit {
 	@ViewChild('board', { static: false }) canvas: ElementRef<HTMLCanvasElement>;
 
 	key: string;
-	x = 0;
-	y = 0;
+	x = 5;
+	y = Math.floor(window.innerHeight / 8);
 
-	@HostListener('document:keypress', ['$event'])
+	@HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) { 
     this.key = event.key;
 		console.log(this.key);
-		let arr: Number[] =  [0,0,0,0];
+		console.log(`x: ${this.x}, y: ${this.y}`)
 		switch(this.key) {
 			case 'w':
-				this.x -= 10;
+				this.y -= speed;
 				break;
-			case 'a':
-				this.y -= 10;
-				break;
+			// case 'a':
+			// 	this.x -= 10;
+			// 	break;
 			case 's':
-				this.x += 10;
+				this.y += speed;
 				break;
-			case 'd':
-				this.y += 10;
-				break;
+			// case 'd':
+			// 	this.x += 10;
+			// 	break;
 		}
+		if (this.y + 60 > this.canvas.nativeElement.height)
+			this.y -= speed;
+			if (this.y < 0)
+			this.y += speed;
 		this.update();
   }
+
+	// timerSub: Subscription;
+	// ngOnInit() : void {
+	// 	this.timerSub = timer(0, 0).pipe(
+	// 		map(() => {
+	// 			this.update();
+	// 		})
+	// 	).subscribe()
+	// }
+	// ngOnDestroy(): void { 
+	// 	this.timerSubscription.unsubscribe(); 
+	// } 
+
+
+	@HostListener('window:resize', ['$event'])
+	onResize() {
+		this.canvas.nativeElement.width = window.innerWidth / 2;
+		this.canvas.nativeElement.height = window.innerHeight / 3;
+	}
 
 	private ctx: CanvasRenderingContext2D|any;
 
 	ngAfterViewInit() : void {
-		this.canvas.nativeElement.width = BOARD_W;
-		this.canvas.nativeElement.height = BOARD_H;
+		this.canvas.nativeElement.width = window.innerWidth / 2;
+		this.canvas.nativeElement.height = window.innerHeight / 3;
 
 		this.ctx = this.canvas.nativeElement.getContext('2d');
-    
+    this.ctx.fillStyle = "black";
+		this.ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+		this.ctx.fillStyle = "white";
+    this.ctx.fillRect(this.x, this.y, 15, 60);
 	}
 
 	update() {
-
-		this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    this.ctx.rect(this.x, this.y, this.x + 30, this.y + 30);
-    this.ctx.stroke();
+		this.ctx.fillStyle = "black";
+		// this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+		this.ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+		this.ctx.fillStyle = "white";
+    this.ctx.fillRect(this.x, this.y, 15, 60);
+    // this.ctx.stroke();
 	}
 
 }
